19be7cf4eee0d5cd863e559ce1ab99186d39135d
vinnichenko-alex <oleksandr.vinncenko@gmail.com>
2023-07-13 10:20:36 +0300

    Add try...except case

--- task1/guess_game.py
+++ task1/guess_game.py
@@ -3,7 +3,11 @@ import random
 
 for i in range(random.randint(1, 10)):
     guess_num = random.randint(1, 10)
-    opinion_num = int(input('Guess the num '))
+    opinion_num = 0
+    try:
+        opinion_num = int(input('Guess the num '))
+    except ValueError:
+        print(f'Problems with type of variable {opinion_num}')
     if opinion_num == guess_num:
         print('You win ')
         break
